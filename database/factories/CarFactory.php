2a9095daa08a2a83d03f563f63662dfb989cd184
fardan <fardan.nozami@gmail.com>
2022-08-14 14:48:49 +0700

    adding logic at carFactory

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -13,11 +13,19 @@ class CarFactory extends Factory
      */
     public function definition()
     {
+        $isSoldOut = $this->faker->boolean();
+        $sold_at = null;
+
+        if($isSoldOut) {
+            $sold_at = date("Y-m-d H:i:s");
+        }
+        
         return [
             'year' => $this->faker->numberBetween(1995, 2022),
             'price' => $this->faker->numberBetween(40000000, 1000000000),
             'color' => $this->faker->safeColorName(),
-            'isSoldOut' => $this->faker->boolean(),
+            'isSoldOut' => $isSoldOut,
+            'sold_at' => $sold_at,
             'engine' => $this->faker->randomElement(['Internal Combustion Engine', 'External Combustion Engine']),
             'capacity' => $this->faker->numberBetween(4, 8),
             'type' => $this->faker->randomElement(['Sedan', 'Hatchback', 'MPV', 'SUV', 'Station Wagon', 'Jeep']),
