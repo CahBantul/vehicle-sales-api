0a17c6ca5995989c6b405d140ca717cb4a27f7f6
fardan <fardan.nozami@gmail.com>
2022-08-14 12:54:15 +0700

    vehicle done

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('vehicle')->group(function (){
   Route::get('/', [VehicleController::class, "index"]);
   Route::get('/count', [VehicleController::class, "count"]);
